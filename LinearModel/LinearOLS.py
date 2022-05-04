499c15cf10d156139f96c95b7be9ef9f2f11ae66
salvatore finizio <salvatorefinizio@salvatores-MacBook-Air.local>
2022-05-04 17:57:26 +0200

    Initial commit

---
+++ LinearModel/LinearOLS.py
@@ -0,0 +1,8 @@
+from LinearModel.LoadData import *
+
+co2_TperCap = loadDataCO2()
+GDP_perCap = loadDataGDP()
+
+reg_ols = sm.OLS(co2_TperCap, GDP_perCap).fit()
+
+print(reg_ols.summary())
---
+++ LinearModel/LoadData.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import statsmodels.api as sm
+
+def loadDataGDP():
+    df=pd.read_csv("/Users/salvatorefinizio/github/dataSets/data_portugal.csv", index_col=0, delimiter=";", decimal=",") #load the csv file ("note: remember the limiter next time")
+
+    GDP_perCap=df.GDP_perCap #regressor
+    GDP_perCap=sm.add_constant(GDP_perCap) #add constant term
+
+    return GDP_perCap
+
+def loadDataCO2():
+    df=pd.read_csv("/Users/salvatorefinizio/github/dataSets/data_portugal.csv", index_col=0, delimiter=";", decimal=",") #load the csv file ("note: remember the limiter next time")
+
+    co2_TperCap=df.Annual_CO2_emissions_TperCap #response variable
+
+    return co2_TperCap
