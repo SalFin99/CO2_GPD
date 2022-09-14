70834a1a70d048b55ca9349e37fc18d5ce22c166
SalFin99 <SalFin.99@gmail.com>
2022-09-14 17:34:23 +0200

    working on dummies and nan values

--- LinearModels/LinearOLS_fixed.py
+++ LinearModels/LinearOLS_fixed.py
@@ -2,6 +2,7 @@ from LoadData import *
 import statsmodels.api as sm
 import pandas as pd
 import numpy as np
+from diagnosticTests import *
 
 data = loadAll() #just returns the database
 
@@ -12,16 +13,20 @@ y=data[['Annual_CO2_emissions_TperCap']] #independet variable
 
 x=data[['GDP_perCap', 'GDP_lag1', 'CO2_lag1']] #regressors
 
-x['dummy']=0 #dummy columns
+x['dummy']=np.nan #dummy columns
 x.loc['1989', 'dummy']=1 #setting 1 for specific years....
 x.loc['2003', 'dummy']=1
 
-x['GDP_lag1']=x['GDP_lag1'].fillna(0) #fixing na values
-x['CO2_lag1']=x['CO2_lag1'].fillna(0)
+#x['GDP_lag1']=x['GDP_lag1'].fillna(0) #fixing na values
+#x['CO2_lag1']=x['CO2_lag1'].fillna(0)
 
 x=sm.add_constant(x)
 
-reg_ols = sm.OLS(y, x).fit()
-reg_ols.get_robustcov_results(cov_type='HC3')
+print(x)
+
+reg_ols = sm.OLS(y, x, missing='drop').fit()
+
 
 print(reg_ols.summary())
+#normalityTest(reg_ols)
+#LMtestAutocorrelation(reg_ols)
